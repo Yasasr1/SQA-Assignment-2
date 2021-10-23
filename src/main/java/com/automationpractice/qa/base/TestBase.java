a7d6a6e8cdce53a129872b433597a754f51155b3
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-10-23 18:44:58 +0530

    login test

--- src/main/java/com/automationpractice/qa/base/TestBase.java
+++ src/main/java/com/automationpractice/qa/base/TestBase.java
@@ -1,4 +1,37 @@
 package com.automationpractice.qa.base;
 
+import com.automationpractice.qa.util.TestUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
+    public static WebDriver driver;
+    public static Properties properties;
+
+    public TestBase(){
+
+        try {
+            properties = new Properties();
+            FileInputStream stream = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/com/automationpractice/qa/config/config.properties");
+            properties.load(stream);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/libs/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        driver.get(properties.getProperty("url"));
+
+    }
+
 }
--- src/main/java/com/automationpractice/qa/pages/LoginPage.java
+++ src/main/java/com/automationpractice/qa/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package com.automationpractice.qa.pages;
 
-public class LoginPage {
+import com.automationpractice.qa.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends TestBase {
+    @FindBy(xpath = "//span[normalize-space()='Sign in']")
+    WebElement loginBtn;
+
+    @FindBy(xpath = "//input[@id='email']")
+    WebElement emailField;
+
+    @FindBy(xpath = "//input[@id='passwd']")
+    WebElement passwordField;
+
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public Boolean validateLoginPage() {
+        return loginBtn.isDisplayed();
+    }
+
+    public AccountPage login(String email, String password) {
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        loginBtn.click();
+        return new AccountPage();
+    }
 }
