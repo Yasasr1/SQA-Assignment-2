0084548fa1ffba00873ab103180af89de7785f69
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-10-23 23:38:28 +0530

    conditional browser selection

--- src/main/java/com/automationpractice/qa/base/TestBase.java
+++ src/main/java/com/automationpractice/qa/base/TestBase.java
@@ -3,9 +3,11 @@ package com.automationpractice.qa.base;
 import com.automationpractice.qa.util.TestUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -25,8 +27,14 @@ public class TestBase {
     }
 
     public static void init() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/libs/chromedriver");
-        driver = new ChromeDriver();
+        if(Objects.equals(System.getProperty("browser"), "chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/libs/chromedriver");
+            driver = new ChromeDriver();
+        } else if(Objects.equals(System.getProperty("browser"), "firefox")) {
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/libs/geckodriver");
+            driver = new FirefoxDriver();
+
+        }
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
